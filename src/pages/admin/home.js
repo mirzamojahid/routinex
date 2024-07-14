12a2fc3ad3246976413d7fd2d9bb2b6ee7383fac
Mirza Mojahid <mojahid360degree@gmail.com>
2024-07-14 15:36:12 +0600

    routine file list api connected

--- src/pages/admin/home.js
+++ src/pages/admin/home.js
@@ -1,26 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card, List } from 'antd'
-import { useDispatch, useSelector } from 'react-redux';
 import { base_endpoint, headerx } from '../../utils/constants';
 import { CheckToken, clearToken } from '../../utils/auth';
-import { routinelistAction } from '../../appstate/actions/routineAction';
 
 function Home() {
 
 
-  const dispatch = useDispatch();
-  const { rouitne_department, rouitne_list } = useSelector((state) => state.routine);
+  const [routineList,setRoutineList]=useState([])
 
   const FetchInfo = async () => {
     try {
       headerx['Authorization'] = `Bearer ${CheckToken()}`;
-      const res = await fetch(base_endpoint + "/api/diu/rouines/", {
+      const res = await fetch(base_endpoint + "/api/diu/api/routine/file/", {
         method: "GET",
         headers: headerx
       })
       const datax = await res.json();
       if (res.status === 200) {
-        dispatch(routinelistAction(datax));
+       setRoutineList(datax['data']);
       } else if (res.status === 401) {
         clearToken();
         window.location.href = "/login";
@@ -32,9 +29,27 @@ function Home() {
   }
 
 
+  const downloadRoutineFile = async (file_name,file_link) => {
+    try {
+        const response = await fetch(file_link);
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', ""+file_name+'routine.xlsx');
+        document.body.appendChild(link);
+        link.click();
+        link.parentNode.removeChild(link);
+    } catch (error) {
+        console.error('Error downloading file:', error);
+        // Handle error as needed
+    }
+
+
+}
 
   useEffect(() => {
-    if (rouitne_list !== null && rouitne_list.length === 0) {
+    if (routineList !== null && routineList.length === 0) {
       FetchInfo();
     }
   }, [])
@@ -42,7 +57,12 @@ function Home() {
   return (
     <div>
       <Card title='Routine List'>
-        <List></List>
+        <List dataSource={routineList} renderItem={(e) => {
+          return <List.Item onClick={()=>{
+
+            downloadRoutineFile(`v_${e.version}_${e.semester}`,e.file);
+          }}>V{e.version}. {e.semester}</List.Item>
+        }}></List>
       </Card>
     </div>
   )
